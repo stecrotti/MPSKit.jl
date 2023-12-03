0d1de4efc003a5794d72caa3df7d5f3021d5dd9c
lkdvos <lukas.devos@ugent.be>
2023-12-03 18:46:08 +0100

    Variable names refactor

--- src/operators/abstractmpo.jl
+++ src/operators/abstractmpo.jl
@@ -14,9 +14,9 @@ left_virtualsize(O::SparseMPOTensor) = size(O, 1)
 right_virtualsize(O::MPOTensor) = 1
 right_virtualsize(O::SparseMPOTensor) = size(O, 4)
 
-function ismpoidentity(O::MPOTensor; tol=eps(scalartype(O))^3 / 4)
+function ismpoidentity(O::MPOTensor{S}; tol=eps(real(scalartype(O)))^3 / 4) where {S}
     O isa BraidingTensor && return true
-    τ = TensorKit.BraidingTensor{S,storagetype(O)}(space(O, 1), space(O, 2))
+    τ = TensorKit.BraidingTensor{S,storagetype(O)}(space(O, 2), space(O, 1))
     return isapprox(O, τ; atol=tol)
 end
 
@@ -37,13 +37,6 @@ TensorKit.space(O::AbstractMPO, i::Int) = physicalspace(O, i)
 left_virtualsize(H::AbstractMPO, i::Int) = left_virtualsize(H[i])
 right_virtualsize(H::AbstractMPO, i::Int) = right_virtualsize(H[i])
 
-"""
-    tensortype(H::Union{O,Type{O}}) where {O<:AbstractMPO}
-
-Return the type of tensors of a Matrix Product Operator.
-"""
-function tensortype end
-tensortype(H::AbstractMPO) = tensortype(typeof(H))
 
 TensorKit.spacetype(::Union{O,Type{O}}) where {O<:AbstractMPO} = spacetype(tensortype(O))
 TensorKit.sectortype(::Union{O,Type{O}}) where {O<:AbstractMPO} = sectortype(tensortype(O))
@@ -51,18 +44,19 @@ function TensorKit.storagetype(::Union{O,Type{O}}) where {O<:AbstractMPO}
     return storagetype(tensortype(O))
 end
 
-VectorInterface.scalartype(::Type{O}) where {O<:AbstractMPO} = scalartype(tensortype(O))
+VectorInterface.scalartype(::Type{O}) where {O<:AbstractMPO} = scalartype(eltype(O))
 
 Base.eltype(O::AbstractMPO) = eltype(parent(O))
-Base.length(a::AbstractMPO) = length(parent(a))
+Base.length(O::AbstractMPO) = length(parent(O))
 
-Base.iterate(x::AbstractMPO, args...) = iterate(parent(x), args...)
+Base.iterate(O::AbstractMPO, args...) = iterate(parent(O), args...)
 
-Base.getindex(a::AbstractMPO, i::Int) = getindex(parent(a), i)
-Base.setindex!(a::AbstractMPO, v, i::Int) = setindex!(parent(a), v, i)
+Base.getindex(O::AbstractMPO, i::Int) = getindex(parent(O), i)
+Base.setindex!(O::AbstractMPO, v, i::Int) = setindex!(parent(O), v, i)
+Base.lastindex(O::AbstractMPO) = lastindex(parent(O))
 Base.checkbounds(::Type{Bool}, O::AbstractMPO, args...) = checkbounds(parent(O), args...)
 
-Base.similar(x::MPO, args...) where {MPO<:AbstractMPO} = MPO(similar(parent(x), args...))
-Base.copy(x::MPO) where {MPO<:AbstractMPO} = MPO(copy(parent(x)))
-Base.deepcopy(x::MPO) where {MPO<:AbstractMPO} = MPO(deepcopy(parent(x)))
-Base.repeat(x::MPO, args...) where {MPO<:AbstractMPO} = MPO(repeat(parent(x), args...))
+Base.similar(O::MPO, args...) where {MPO<:AbstractMPO} = MPO(similar(parent(O), args...))
+Base.copy(O::MPO) where {MPO<:AbstractMPO} = MPO(copy(parent(O)))
+Base.deepcopy(O::MPO) where {MPO<:AbstractMPO} = MPO(deepcopy(parent(O)))
+Base.repeat(O::MPO, args...) where {MPO<:AbstractMPO} = MPO(repeat(parent(O), args...))
