ce916e55e7acbd9c92a1b1b7ab2b253bac95e047
lkdvos <lukas.devos@ugent.be>
2023-12-03 18:46:34 +0100

    Remove stray semicolons

--- src/transfermatrix/transfermatrix.jl
+++ src/transfermatrix/transfermatrix.jl
@@ -1,4 +1,4 @@
-abstract type AbstractTransferMatrix end;
+abstract type AbstractTransferMatrix end
 
 # single site transfer
 struct SingleTransferMatrix{A<:AbstractTensorMap,B,C<:AbstractTensorMap} <:
@@ -14,16 +14,16 @@ struct ProductTransferMatrix{T<:AbstractTransferMatrix} <: AbstractTransferMatri
     tms::Vector{T} # I don't want to use tuples, as an infinite mps transfer matrix will then be non-inferable
 end
 
-ProductTransferMatrix(v::AbstractVector) = ProductTransferMatrix(convert(Vector, v));
+ProductTransferMatrix(v::AbstractVector) = ProductTransferMatrix(convert(Vector, v))
 
 # a subset of possible operations, but certainly not all of them
 function Base.:*(prod::ProductTransferMatrix{T}, tm::T) where {T<:AbstractTransferMatrix}
     return ProductTransferMatrix(vcat(prod.tms, tm))
-end;
+end
 function Base.:*(tm::T, prod::ProductTransferMatrix{T}) where {T<:AbstractTransferMatrix}
     return ProductTransferMatrix(vcat(prod.tms, tm))
-end;
-Base.:*(tm1::T, tm2::T) where {T<:SingleTransferMatrix} = ProductTransferMatrix([tm1, tm2]);
+end
+Base.:*(tm1::T, tm2::T) where {T<:SingleTransferMatrix} = ProductTransferMatrix([tm1, tm2])
 
 # regularized transfer matrices; where we project out after every full application
 struct RegTransferMatrix{T<:AbstractTransferMatrix,L,R} <: AbstractTransferMatrix
@@ -32,34 +32,34 @@ struct RegTransferMatrix{T<:AbstractTransferMatrix,L,R} <: AbstractTransferMatri
     rvec::R
 end
 
-#flip em
+# flip em
 function TensorKit.flip(tm::SingleTransferMatrix)
     return SingleTransferMatrix(tm.above, tm.middle, tm.below, !tm.isflipped)
-end;
-TensorKit.flip(tm::ProductTransferMatrix) = ProductTransferMatrix(flip.(reverse(tm.tms)));
-TensorKit.flip(tm::RegTransferMatrix) = RegTransferMatrix(flip(tm.tm), tm.rvec, tm.lvec);
+end
+TensorKit.flip(tm::ProductTransferMatrix) = ProductTransferMatrix(flip.(reverse(tm.tms)))
+TensorKit.flip(tm::RegTransferMatrix) = RegTransferMatrix(flip(tm.tm), tm.rvec, tm.lvec)
 
 # TransferMatrix acting on a vector using *
-Base.:*(tm::AbstractTransferMatrix, vec) = tm(vec);
-Base.:*(vec, tm::AbstractTransferMatrix) = flip(tm)(vec);
+Base.:*(tm::AbstractTransferMatrix, vec) = tm(vec)
+Base.:*(vec, tm::AbstractTransferMatrix) = flip(tm)(vec)
 
 # TransferMatrix acting as a function
-(d::ProductTransferMatrix)(vec) = foldr((a, b) -> a(b), d.tms; init=vec);
+(d::ProductTransferMatrix)(vec) = foldr((a, b) -> a(b), d.tms; init=vec)
 function (d::SingleTransferMatrix)(vec)
     return if d.isflipped
         transfer_left(vec, d.middle, d.above, d.below)
     else
         transfer_right(vec, d.middle, d.above, d.below)
     end
-end;
-(d::RegTransferMatrix)(vec) = regularize!(d.tm * vec, d.lvec, d.rvec);
+end
+(d::RegTransferMatrix)(vec) = regularize!(d.tm * vec, d.lvec, d.rvec)
 
 # constructors
-TransferMatrix(a) = TransferMatrix(a, nothing, a);
-TransferMatrix(a, b) = TransferMatrix(a, nothing, b);
+TransferMatrix(a) = TransferMatrix(a, nothing, a)
+TransferMatrix(a, b) = TransferMatrix(a, nothing, b)
 function TransferMatrix(a::AbstractTensorMap, b, c::AbstractTensorMap, isflipped=false)
     return SingleTransferMatrix(a, b, c, isflipped)
-end;
+end
 function TransferMatrix(a::AbstractVector, b, c::AbstractVector, isflipped=false)
     tot = ProductTransferMatrix(convert(Vector, TransferMatrix.(a, b, c)))
     return isflipped ? flip(tot) : tot
